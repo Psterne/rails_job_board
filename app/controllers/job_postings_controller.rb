3f37ac0df67ea83c751de8637bc91cf833bb5ef6
Psterne <pilarsterne@gmail.com>
2016-12-15 07:35:29 -0800

    added options to sort posts by date or name

--- app/controllers/job_postings_controller.rb
+++ app/controllers/job_postings_controller.rb
@@ -1,10 +1,11 @@
 class JobPostingsController < ApplicationController
   before_action :set_job_posting, only: [:show, :edit, :update, :destroy]
+  helper_method :sort_column, :sort_direction
 
   # GET /job_postings
   # GET /job_postings.json
   def index
-    @job_postings = JobPosting.all
+    @job_postings = JobPosting.order(sort_column + " " + sort_direction)
   end
 
   # GET /job_postings/1
@@ -29,11 +30,11 @@ class JobPostingsController < ApplicationController
 
     respond_to do |format|
       if @job_posting.save
-        format.html { redirect_to @job_posting, notice: 'Job posting was successfully created.' }
+        format.html { redirect_to job_postings_url, notice: 'Job posting was successfully created.' }
         format.json { render :show, status: :created, location: @job_posting }
       else
         format.json { render json: @job_posting.errors, status: :unprocessable_entity }
-        format.html { render :new, notice: 'Job posting was not created.' }
+        format.html { redirect_to job_postings_url, notice: 'Job posting was not created.' }
       end
     end
   end
@@ -43,10 +44,10 @@ class JobPostingsController < ApplicationController
   def update
     respond_to do |format|
       if @job_posting.update(job_posting_params)
-        format.html { redirect_to @job_posting, notice: 'Job posting was successfully updated.' }
+        format.html { redirect_to job_postings_url, notice: 'Job posting was successfully updated.' }
         format.json { render :show, status: :ok, location: @job_posting }
       else
-        format.html { render :edit }
+        format.html { redirect_to job_postings_url, notice: 'Job posting could not be updated.' }
         format.json { render json: @job_posting.errors, status: :unprocessable_entity }
       end
     end
@@ -71,5 +72,13 @@ class JobPostingsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def job_posting_params
       params.require(:job_posting).permit(:title, :description, :hiring_manager, :email)
+    end
+
+    def sort_column ## this sets the default for index page
+      JobPosting.column_names.include?(params[:sort]) ? params[:sort] : "updated_at"
+    end
+
+    def sort_direction ## this tells page to display in descending order by default
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
     end
 end
